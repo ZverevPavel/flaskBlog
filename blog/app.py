a92ec93ad489f0acc14841f024a92bc0af02ae83
ZverevPavel <zzzverevv@yandex.ru>
2023-03-13 20:09:34 +0500

    task_2_ok

--- blog/app.py
+++ blog/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from blog.views.users import users_app
+from blog.views.articles import articles_app
 
 app = Flask(__name__)
 
@@ -10,4 +11,4 @@ def index():
 
 
 app.register_blueprint(users_app, url_prefix="/users")
-
+app.register_blueprint(articles_app, url_prefix="/articles")
